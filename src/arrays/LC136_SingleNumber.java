1a9173992d191f1fcb4cbb4ee9a712d598d06a2e
waterlili1883 <lila1719au@gmail.com>
2025-09-02 22:35:09 +1000

    feat(arrays): solve LC136_SingleNumber using bit manipulation

---
+++ src/arrays/LC136_SingleNumber.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class LC136_SingleNumber {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            res ^= num;
+        }
+        return res;
+    }
+}
